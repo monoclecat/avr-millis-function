ca564ddfb2c62f8f267331568026b63fb4cedb3a
Andy Delay <contact@monoclecat.de>
2015-01-26 22:12:02 +0100

    A millis() function for the AVR
    Returns the time in milliseconds that has passed since program start.

---
+++ millis_example.c
@@ -0,0 +1,30 @@
+/* 
+Name: Timer demo
+Author: Monoclecat
+
+This demo will make the pin PORTB5 (pin 13 on the Arduino board) toggle every second
+*/
+
+#include <millis.c>
+
+
+
+int main()
+{
+  DDRB = (1 << DDB5); //setting PORTB5 to an output
+  PORTB ^= (1 << PORTB5);
+  
+  init_millis(16000000UL); //frequency the atmega328p is running at
+  unsigned long prev_millis; //the last time the led was toggled
+  prev_millis = millis(); 
+  sei();
+  for(;;)
+  {
+    if (millis() - prev_millis > 1000)
+    {
+      PORTB ^= (1 << PORTB5); //Turn on / Turn off the LED
+      prev_millis = millis();
+    }
+  }
+  return 0;
+}
